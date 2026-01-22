11227e89bd908317f43622169416655cdf98e18f
TBS96 <9tbs6@proton.me>
2026-01-22 18:17:51 +0530

    refactor: update package.json scripts and improve type safety in configuration
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import express, { type Application } from 'express'
+import express from 'express'
+import type { Application, Request, Response } from 'express'
 
 const app: Application = express();
 
@@ -7,5 +8,13 @@ app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 
 app.use(express.static('public'));
+
+app.get('/', (req: Request, res: Response) => {
+    return res
+    .status(200)
+    .json({
+        name: 'Prantik'
+    })
+})
 
 export { app };
